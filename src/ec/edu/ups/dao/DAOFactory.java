352ccb23fbb9cb6eb2f8c55778009cd610faa3d2
Evelyn Pintado <pinty_eq28@hotmail.com>
2020-05-03 17:32:13 -0500

    Script Base de datos

---
+++ src/ec/edu/ups/dao/DAOFactory.java
@@ -0,0 +1,9 @@
+package ec.edu.ups.dao;
+
+public class DAOFactory {
+
+	public DAOFactory() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/ec/edu/ups/modelo/Telefono.java
@@ -0,0 +1,9 @@
+package ec.edu.ups.modelo;
+
+public class Telefono {
+
+	public Telefono() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
